58d93c909216fcbf183f3d051ad8407333fc38f3
fightfastest <fightfastest@github.com>
2018-09-25 06:25:39 +0800

    lixg

---
+++ src/components/login/login.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="login">
+   <div class="loginWap">
+     <div>LOGIN FORM</div>
+
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .login{
+    width: 100%;
+    height: 100%;
+    background-color: #2d3a4b;
+    position: relative;
+    .loginWap{
+      width: 42%;
+      margin-left: 29%;
+      height: 60%;
+      background: red;
+      position: absolute;
+      top: 20%;
+      left: 0;
+    }
+  }
+</style>
